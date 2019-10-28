a386cdfa665c1d6cbd3708317fc6c9b7ec644a27
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-10-28 18:40:53 -0400

    Updates to problem 904
    Removed output streams and repetitive comments
    Improved the algorithm the algorithm a bit
    1. Uses closure
    2. Removed useless captures

--- leetcode_rust/src/problem_904.rs
+++ leetcode_rust/src/problem_904.rs
@@ -1,108 +1,80 @@
 use crate::Solution;
 use std::cmp::Ordering;
-use std::collections::HashMap;
 
 impl Solution {
     pub fn total_fruit(tree: Vec<i32>) -> i32 {
         // TODO ::  Find the first 2 couple.
         //          We can actually exit early based on this algorithm.
         //          For now we will just use HashMap
+        // TODO ::  Calibrating the global maximum stuff can be extracted into a closure...
         match tree.len() {
             0 => 0, // vector is empty...exit early
             _ => {
-                // map : key value => (frequency, first index)
-                let mut fruits: std::collections::HashMap<i32, (i32, i32)> =
+                // TODO ::  To prevent additional indirection, this HashMap could be turned into a static array...
+                // NOTE ::  `basket` maps f : key value => (frequency, first index)
+                // Local variables
+                let mut basket: std::collections::HashMap<i32, (i32, i32)> =
                     std::collections::HashMap::new();
                 let mut index: i32 = 0;
                 let mut global_frequency: i32 = 0;
+
+                // Helper closures
+                let mut update_global_frequency =
+                    |p_basket: &std::collections::HashMap<i32, (i32, i32)>, p_freq: &mut i32| {
+                        let local_freq_sum =
+                            p_basket.iter().fold(0, |acc, (_, (freq, _))| acc + freq);
+                        match local_freq_sum.cmp(&p_freq) {
+                            Ordering::Greater => {
+                                *p_freq = local_freq_sum;
+                            }
+                            _ => {}
+                        }
+                    };
+                // Perform algorithm
                 tree.iter().for_each(|&iter| {
-                    println!("iter:{}", iter);
-                    match fruits.len() {
+                    match basket.len() {
                         0 | 1 => {
-                            // There are only 0 or 1 unique keys in the map, no need to check.
-                            println!("map contains {} unique key", fruits.len());
-                            match fruits.get_mut(&iter) {
+                            match basket.get_mut(&iter) {
                                 Some(some) => {
-                                    println!(
-                                        "already contains the key:{} must increment the frequency",
-                                        iter
-                                    );
                                     (*some).0 = (*some).0 + 1;
                                 }
                                 None => {
-                                    println!("the key:{} does not exist, inserting...", iter);
-                                    fruits.insert(iter, (1, index));
+                                    basket.insert(iter, (1, index));
                                 }
                             }
                         }
                         2 => {
-                            // There are 2 keys in the map, must check if current is also not unique.
-                            println!("map contains exactly {} unique keys...", fruits.len());
-                            match fruits.get_mut(&iter) {
+                            match basket.get_mut(&iter) {
                                 Some(some) => {
-                                    println!(
-                                        "already contains the key:{} with freq:{} must increment the frequency",
-                                        iter,
-                                        (*some).0
-                                    );
                                     (*some).0 = (*some).0 + 1;
                                 }
                                 None => {
-                                    println!(
-                                        "the key:{} does not exist, must perform cleanup...",
-                                        iter
+                                    update_global_frequency(&basket, &mut global_frequency);
+                                    let oldest_key = basket.iter().min_by(
+                                        |(_, (_, left_index)), (_, (_, right_index))| {
+                                            left_index.cmp(right_index)
+                                        },
                                     );
-                                    let local_freq_sum = fruits.iter().fold(0, |acc, (value, (freq, index))|{
-                                        println!("value:{} at index:{} have freq:{}",value, index, freq);
-                                        acc + freq
-                                    });
-                                    println!("global max is {} while local max is {}", global_frequency, local_freq_sum);
-                                    match local_freq_sum.cmp(&global_frequency) {
-                                        Ordering::Greater => {
-                                            global_frequency = local_freq_sum;
-                                            println!("new global max frequency is {}", global_frequency);
-                                        },
-                                        _ => {}
-                                    }
-                                    let oldest_key = fruits.iter().min_by(|(_, (_, left_index)),(_, (_, right_index))|{
-                                        left_index.cmp(right_index)
-                                    });
                                     match oldest_key {
-                                        Some((&old_key, &(old_freq, old_index))) =>{
-                                             // NOTE :: This was actually problematic, but Rust type system catched this error :)
-                                            println!("oldest key in map is {} with value:{} and freq:{}", old_key, old_freq, old_index);
-                                            fruits.remove(&old_key);
-                                            fruits.insert(iter, (1, index));
-                                        },
-                                        None=> {
-                                            panic!("cannot find the oldest key in map...");
+                                        Some((&old_key, &(_, _))) => {
+                                            // NOTE :: This was actually problematic, but Rust type system catched this error :)
+                                            basket.remove(&old_key);
+                                            basket.insert(iter, (1, index));
+                                        }
+                                        None => {
+                                            panic!("cannot find the oldest key in map. This is possible if the basket is empty somehow...");
                                         }
                                     }
                                 }
                             }
                         }
-                        _ => panic!("unhandled case..."),
+                        _ => panic!(
+                            "the basket appear to have more than 2 unique keys in it. You messed up..."
+                        ),
                     }
-                    println!();
                     index = index + 1;
                 });
-                // FIXME :: Must recalculate the global maximum frequency again.
-                //          This (and some more functions) can be made into closures.
-                let local_freq_sum = fruits.iter().fold(0, |acc, (value, (freq, index))| {
-                    println!("value:{} at index:{} have freq:{}", value, index, freq);
-                    acc + freq
-                });
-                println!(
-                    "global max is {} while local max is {}",
-                    global_frequency, local_freq_sum
-                );
-                match local_freq_sum.cmp(&global_frequency) {
-                    Ordering::Greater => {
-                        global_frequency = local_freq_sum;
-                        println!("new global max frequency is {}", global_frequency);
-                    }
-                    _ => {}
-                }
+                update_global_frequency(&basket, &mut global_frequency);
                 global_frequency
             }
         }
