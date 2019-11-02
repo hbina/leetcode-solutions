5780de93dedc4f0fbb8e0df7f6f437e3371d6bd6
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-11-02 11:06:44 -0400

    Solved problem 96

--- leetcode_rust/src/problem_96.rs
+++ leetcode_rust/src/problem_96.rs
@@ -1,51 +1,17 @@
 use crate::Solution;
-use std::ops::Add;
 
 impl Solution {
     pub fn num_trees(n: i32) -> i32 {
-        let arr = (1..=n).collect::<Vec<_>>();
-        arr.iter().fold(0, |acc, &middle| {
-            // Accumulate left and right
-            let left_begin = 1;
-            let left_end = middle - 1;
-            let right_begin = middle + 1;
-            let right_end = n;
-
-            println!("middle:{} left:({},{}) right:({},{})", middle, left_begin, left_end, right_begin, right_end);
-            acc + internal_summation(left_begin, left_end) + internal_summation(right_begin, right_end)
-        })
-    }
-}
-
-fn internal_summation(left: i32, right: i32) -> i32 {
-    // Evaluate left and right
-    println!("left:{} right:{}", left, right);
-    match left.cmp(&right) {
-        std::cmp::Ordering::Greater => {
-            println!("left > right --> returning 0");
-            0
-        }
-        std::cmp::Ordering::Equal => {
-            println!("left == right --> returning 1");
-            1
-        }
-        std::cmp::Ordering::Less => {
-            println!("left < right --> evaluating...");
-            if right - left <= 3 {
-                right - left
-            } else {
-                let arr = (left..=right).collect::<Vec<_>>();
-                arr.iter().fold(0, |acc, &middle| {
-                    // Accumulate left and right
-                    let left_begin = left;
-                    let left_end = middle - left;
-                    let right_begin = middle + left;
-                    let right_end = right;
-
-                    println!("middle:{} left:({},{}) right:({},{})", middle, left_begin, left_end, right_begin, right_end);
-                    acc + internal_summation(left_begin, left_end) + internal_summation(right_begin, right_end)
-                })
-            }
+        match n {
+            0 => 1,
+            1 => 1,
+            2 => 2,
+            3 => 5,
+            _ => (1..=n).collect::<Vec<i32>>().iter().fold(0, |acc, &x| {
+                let left = Solution::num_trees(x - 1);
+                let right = Solution::num_trees(n - x);
+                acc + left * right
+            }),
         }
     }
 }
@@ -53,4 +19,6 @@ fn internal_summation(left: i32, right: i32) -> i32 {
 #[test]
 fn test() {
     assert_eq!(Solution::num_trees(3), 5);
+    assert_eq!(Solution::num_trees(4), 14);
+    assert_eq!(Solution::num_trees(5), 42);
 }
