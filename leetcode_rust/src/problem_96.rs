7dd389d1aa4aea196c4b045ea75d4e104c72253a
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-11-02 06:43:36 -0400

    Attempt 1 at problem 96

--- leetcode_rust/src/problem_96.rs
+++ leetcode_rust/src/problem_96.rs
@@ -4,29 +4,53 @@ use std::ops::Add;
 impl Solution {
     pub fn num_trees(n: i32) -> i32 {
         let arr = (1..=n).collect::<Vec<_>>();
-        arr.iter().fold(0, |acc, &x| {
+        arr.iter().fold(0, |acc, &middle| {
             // Accumulate left and right
-            match x {
-                1 => {}
-                => {}
-            }
-            let left = (1..=x).collect::<Vec<_>>();
-            let right = (x..=n).collect::<Vec<_>>();
-            println!("left:{:?} right:{:?}", left, right);
-            acc + internal_summation(&left) + internal_summation(&right)
+            let left_begin = 1;
+            let left_end = middle - 1;
+            let right_begin = middle + 1;
+            let right_end = n;
+
+            println!("middle:{} left:({},{}) right:({},{})", middle, left_begin, left_end, right_begin, right_end);
+            acc + internal_summation(left_begin, left_end) + internal_summation(right_begin, right_end)
         })
     }
 }
 
-fn internal_summation(arr: &Vec<i32>) -> i32 {
+fn internal_summation(left: i32, right: i32) -> i32 {
     // Evaluate left and right
-    match arr.len() {
-        1 => 1,
-        _ => 10
+    println!("left:{} right:{}", left, right);
+    match left.cmp(&right) {
+        std::cmp::Ordering::Greater => {
+            println!("left > right --> returning 0");
+            0
+        }
+        std::cmp::Ordering::Equal => {
+            println!("left == right --> returning 1");
+            1
+        }
+        std::cmp::Ordering::Less => {
+            println!("left < right --> evaluating...");
+            if right - left <= 3 {
+                right - left
+            } else {
+                let arr = (left..=right).collect::<Vec<_>>();
+                arr.iter().fold(0, |acc, &middle| {
+                    // Accumulate left and right
+                    let left_begin = left;
+                    let left_end = middle - left;
+                    let right_begin = middle + left;
+                    let right_end = right;
+
+                    println!("middle:{} left:({},{}) right:({},{})", middle, left_begin, left_end, right_begin, right_end);
+                    acc + internal_summation(left_begin, left_end) + internal_summation(right_begin, right_end)
+                })
+            }
+        }
     }
 }
 
 #[test]
 fn test() {
-    assert_eq!(Solution::num_trees(4), 10);
+    assert_eq!(Solution::num_trees(3), 5);
 }
