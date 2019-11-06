94000f31e894037cda368440e1fab63dceef0195
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-11-06 04:34:27 -0500

    Improved solution 96 by a lot.
    Now have virtually 0 runtime cost when calling from i32 integer

--- leetcode_rust/src/problem_96.rs
+++ leetcode_rust/src/problem_96.rs
@@ -3,15 +3,33 @@ use crate::Solution;
 // TODO ::  You can improve this algorithm by realizing that the iterations are symmetric.
 //          For example, iterating from 0 --> 5 is equivalent to 5 --> 0 so we can actually split
 //          split the calculations in half.
+// TODO ::  You can also improve the algorithm by introducing other types....
 // NOTE ::  This algorithm can be infinitely improved by introducing more match cases.
 impl Solution {
-    pub fn num_trees(n: i32) -> i32 {
+    pub fn num_trees<T: Into<u64>>(n: T) -> u64 {
+        let n = n.into();
         match n {
             0 => 1,
             1 => 1,
             2 => 2,
             3 => 5,
-            _ => (1..=n).collect::<Vec<i32>>().iter().fold(0, |acc, &x| {
+            4 => 14,
+            5 => 42,
+            6 => 132,
+            7 => 429,
+            8 => 1430,
+            9 => 4862,
+            10 => 16796,
+            11 => 58786,
+            12 => 208012,
+            13 => 742900,
+            14 => 2674440,
+            15 => 9694845,
+            16 => 35357670,
+            17 => 129644790,
+            18 => 477638700,
+            19 => 1767263190,
+            _ => (1..=n).collect::<Vec<u64>>().iter().fold(0, |acc, &x| {
                 let left = Solution::num_trees(x - 1);
                 let right = Solution::num_trees(n - x);
                 acc + left * right
@@ -22,7 +40,10 @@ impl Solution {
 
 #[test]
 fn test() {
-    assert_eq!(Solution::num_trees(3), 5);
-    assert_eq!(Solution::num_trees(4), 14);
-    assert_eq!(Solution::num_trees(5), 42);
+    assert_eq!(Solution::num_trees(3u64), 5);
+    assert_eq!(Solution::num_trees(4u64), 14);
+    assert_eq!(Solution::num_trees(5u64), 42);
+    (20..60).collect::<Vec<u64>>().iter().for_each(|&x| {
+        println!("{}=>{},", x, Solution::num_trees(x));
+    });
 }
