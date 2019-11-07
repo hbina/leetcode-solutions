385222a6593aa4d5451b296a8a10d7eedd9c2c8e
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-11-06 22:21:45 -0500

    Added DP solution to problem_1025

--- leetcode_rust/src/problem_1025.rs
+++ leetcode_rust/src/problem_1025.rs
@@ -4,6 +4,18 @@ impl Solution {
     pub fn divisor_game(n: i32) -> bool {
         n % 2 == 0
     }
+
+    pub fn divisor_game_dp(n: i32) -> bool {
+        match n {
+            1 => false,
+            2 => true,
+            3 => false,
+            _ => (1..n / 2)
+                .collect::<Vec<_>>()
+                .iter()
+                .fold(false, |acc, &x| acc | !Solution::divisor_game_dp(n - x)),
+        }
+    }
 }
 
 #[test]
@@ -13,4 +25,10 @@ fn test() {
     assert_eq!(Solution::divisor_game(3), false);
     assert_eq!(Solution::divisor_game(4), true);
     assert_eq!(Solution::divisor_game(5), false);
+
+    assert_eq!(Solution::divisor_game_dp(1), false);
+    assert_eq!(Solution::divisor_game_dp(2), true);
+    assert_eq!(Solution::divisor_game_dp(3), false);
+    assert_eq!(Solution::divisor_game_dp(4), true);
+    assert_eq!(Solution::divisor_game_dp(5), false);
 }
