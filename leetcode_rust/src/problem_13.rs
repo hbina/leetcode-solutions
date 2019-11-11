ec42ab2f1a513cd6f32e9baaa516f229ecc5594d
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-11-11 13:44:06 -0500

    Solved problem

--- leetcode_rust/src/problem_13.rs
+++ leetcode_rust/src/problem_13.rs
@@ -2,8 +2,8 @@ use crate::Solution;
 
 impl Solution {
     pub fn roman_to_int<T>(s: T) -> i32
-        where
-            T: Into<String>,
+    where
+        T: Into<String>,
     {
         0
     }
