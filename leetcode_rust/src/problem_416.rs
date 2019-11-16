e70d11dfaa0330522eaa3497f73c3b471c07fa67
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-11-16 17:26:54 -0500

    Solved problem 322 and problem 416

--- leetcode_rust/src/problem_416.rs
+++ leetcode_rust/src/problem_416.rs
@@ -2,7 +2,7 @@ use crate::Solution;
 
 impl Solution {
     //  NOTE    ::  Naive implementation, simply collect the permutations as we go.
-    pub fn can_partition_v1(nums: Vec<i32>) -> bool {
+    pub fn can_partition_naive(nums: Vec<i32>) -> bool {
         let mut sum: i32 = nums.iter().sum();
         let mut result: Vec<(i32, i32)> = Vec::new();
         nums.iter().for_each(|&x| match result.len() {
@@ -31,7 +31,7 @@ impl Solution {
 
     //  NOTE    ::  Checks if no possible combination exists. We do this by taking the absolute
     //              difference and checking if its more than the sum of the whats left.
-    pub fn can_partition_v2(nums: Vec<i32>) -> bool {
+    pub fn can_partition_whats_left(nums: Vec<i32>) -> bool {
         let mut sum: i32 = nums.iter().sum();
         let mut result: Vec<(i32, i32)> = Vec::new();
         nums.iter().for_each(|&x| match result.len() {
@@ -62,24 +62,19 @@ impl Solution {
     }
 
     //  NOTE    ::  This function cannot produce false positive!
-    pub fn can_partition_v3(mut nums: Vec<i32>) -> bool {
+    pub fn can_partition_sort_and_try(mut nums: Vec<i32>) -> bool {
         nums.sort_by(|x, y| y.cmp(x));
-        nums.iter().fold(0, |acc, &x| {
-            println!("acc:{}", acc);
-            match acc > 0 {
-                true => acc - x,
-                false => acc + x,
-            }
+        nums.iter().fold(0, |acc, &x| match acc > 0 {
+            true => acc - x,
+            false => acc + x,
         }) == 0
     }
 
-    pub fn can_partition(nums: Vec<i32>) -> bool {
+    //  NOTE    ::  This implementation is horse-crap :3
+    pub fn can_partition_odd_even_counter(nums: Vec<i32>) -> bool {
         // Must sanitize
         let sum: i32 = nums.iter().sum();
-        let sanitize = nums.iter().fold(true, |acc, &x| {
-            println!("sum-x:{} sum:{} x:{}", sum - x, sum, x);
-            acc && (sum - x) >= x
-        });
+        let sanitize = nums.iter().fold(true, |acc, &x| acc && (sum - x) >= x);
         if !sanitize {
             return false;
         }
@@ -94,24 +89,79 @@ impl Solution {
                 twos += (x - 1) / 2;
             }
         });
-        println!("before twos:{} ones:{}", twos, ones);
         if twos % 2 != 0 && ones >= 2 {
             twos -= 1;
             ones -= 2;
         }
-        println!("after twos:{} ones:{}", twos, ones);
         twos % 2 == 0 && ones % 2 == 0
+    }
+
+    pub fn can_partition_dp_top_down(
+        nums: &Vec<i32>,
+        start_from: usize,
+        whats_left: i32,
+        goal_amount: i32,
+    ) -> bool {
+        if start_from >= nums.len() {
+            return false;
+        } else if goal_amount > whats_left {
+            return false;
+        } else if goal_amount == whats_left {
+            return true;
+        } else {
+            let next_start_from = start_from + 1;
+            let value = nums[start_from];
+            let whats_left = whats_left - value;
+            match goal_amount.cmp(&value) {
+                std::cmp::Ordering::Greater => {
+                    return Solution::can_partition_dp_top_down(
+                        &nums,
+                        next_start_from,
+                        whats_left,
+                        goal_amount - value,
+                    ) || Solution::can_partition_dp_top_down(
+                        &nums,
+                        next_start_from,
+                        whats_left,
+                        goal_amount,
+                    );
+                }
+                std::cmp::Ordering::Equal => {
+                    return true;
+                }
+                std::cmp::Ordering::Less => {
+                    return Solution::can_partition_dp_top_down(
+                        &nums,
+                        next_start_from,
+                        whats_left,
+                        goal_amount,
+                    );
+                }
+            }
+        }
+    }
+
+    pub fn can_partition(mut nums: Vec<i32>) -> bool {
+        let sum: i32 = nums.iter().sum();
+        if sum % 2 != 0 {
+            false
+        } else {
+            nums.sort_by(|x, y| y.cmp(x));
+            Solution::can_partition_dp_top_down(&nums, 0, sum, sum / 2)
+        }
     }
 }
 
 #[test]
-fn test() {
-    // assert_eq!(Solution::can_partition(vec![1, 5, 11, 5]), true);
-    // assert_eq!(Solution::can_partition(vec![5, 3, 4, 4]), true);
-    // assert_eq!(Solution::can_partition(vec![3, 3, 1, 94]), false);
-    // assert_eq!(Solution::can_partition(vec![5, 3, 1, 4]), false);
+fn problem_416_test() {
+    assert_eq!(Solution::can_partition(vec![1, 5, 11, 5]), true);
+    assert_eq!(Solution::can_partition(vec![5, 3, 4, 4]), true);
+    assert_eq!(Solution::can_partition(vec![3, 3, 1, 94]), false);
+    assert_eq!(Solution::can_partition(vec![5, 3, 1, 4]), false);
+    assert_eq!(Solution::can_partition(vec![3, 3, 3, 4, 5]), true);
+    assert_eq!(Solution::can_partition(vec![1, 2, 5]), false);
     assert_eq!(
-        Solution::can_partition_v1(vec![
+        Solution::can_partition(vec![
             100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
             100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
             100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
@@ -121,11 +171,17 @@ fn test() {
         ]),
         true
     );
-    assert_eq!(Solution::can_partition(vec![3, 3, 3, 4, 5]), true);
-    assert_eq!(Solution::can_partition(vec![1, 2, 5]), false);
+    assert_eq!(Solution::can_partition(vec![2, 2, 3, 5]), false);
+    assert_eq!(
+        Solution::can_partition(vec![
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 100
+        ]),
+        false
+    );
 }
 
 #[test]
-fn failing() {
-    assert_eq!(Solution::can_partition(vec![2, 2, 3, 5]), false);
-}
+pub fn problem_416_failing() {}
