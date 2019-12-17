67963d601d1d8c41601dbaed4d49434764085174
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-12-17 06:19:39 -0500

    Implement a zip algorithm

---
+++ leetcode_cpp/util/generic/zip.hpp
@@ -0,0 +1,28 @@
+#include <iterator>
+
+namespace util
+{
+namespace generic
+{
+
+template <
+    typename IteratorLhs,
+    typename IteratorRhs,
+    typename BinaryFunctor>
+void zip(
+    IteratorLhs iter_lhs_begin,
+    IteratorLhs iter_lhs_end,
+    IteratorRhs iter_rhs_begin,
+    IteratorRhs iter_rhs_end,
+    const BinaryFunctor &functor)
+{
+    while (iter_lhs_begin != iter_lhs_end && iter_rhs_begin != iter_rhs_end)
+    {
+        functor(*iter_lhs_begin, *iter_rhs_begin);
+        iter_lhs_begin++;
+        iter_rhs_begin++;
+    }
+}
+
+} // namespace generic
+} // namespace util
