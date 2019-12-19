823256b741e7d005f695040eb84347062de25f29
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-12-18 19:58:32 -0500

    Reworked problem 88 to be simpler

--- leetcode_cpp/solutions/problem_88.cpp
+++ leetcode_cpp/solutions/problem_88.cpp
@@ -1,26 +1,27 @@
 #include "doctest/doctest.h"
 
+#include "../util/generic/zip.hpp"
+
+#include <iterator>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 
 template <typename T>
 void merge_simple(
-    std::vector<T> &nums1, std::size_t m,
-    const std::vector<T> &nums2, std::size_t n)
+    std::vector<T> &lhs, std::size_t m,
+    const std::vector<T> &rhs, std::size_t n)
 {
-    for (std::size_t counter = 0; counter < n; counter++)
-    {
-        nums1[m + counter] = nums2[counter];
-    }
-    std::sort(nums1.begin(), nums1.end());
+    lhs.resize(n);
+    lhs.insert(lhs.begin() + m, rhs.cbegin(), rhs.cend());
+    std::sort(lhs.begin(), lhs.end());
 }
 
 TEST_CASE("problem_88")
 {
     const std::vector<int> expected = {1, 2, 2, 3, 5, 6};
-    std::vector<int> nums1 = {1, 2, 3, 0, 0, 0};
-    const std::vector<int> nums2 = {2, 5, 6};
-    merge_simple(nums1, 3, nums2, 3);
-    CHECK(expected == nums1);
+    std::vector<int> input_1 = {1, 2, 3, 0, 0, 0};
+    const std::vector<int> input_2 = {2, 5, 6};
+    merge_simple(input_1, 3, input_2, 3);
+    CHECK(expected == input_1);
 }
