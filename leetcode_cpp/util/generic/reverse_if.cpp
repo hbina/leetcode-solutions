9d4d07b3cdbe8c0f292ab9916a72b5efd9b54cc2
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-12-24 10:49:27 -0500

    Forgot to perform the checks...
    woops

--- leetcode_cpp/util/generic/reverse_if.cpp
+++ leetcode_cpp/util/generic/reverse_if.cpp
@@ -3,12 +3,24 @@
 #include "reverse_if.hpp"
 
 #include <string>
+#include <vector>
 
 TEST_CASE("util::generic::reverse_if")
 {
     std::string input = "h4ll5";
     std::string expected = "h5ll4";
     util::generic::reverse_if(input.begin(), input.end(), std::isdigit);
+    CHECK(expected == input);
+}
+
+TEST_CASE("util::generic::reverse_if")
+{
+    std::vector<int> input = {1, 2, 3, 4, 5};
+    std::vector<int> expected = {1, 4, 3, 2, 5};
+    util::generic::reverse_if(input.begin(), input.end(), [](const int &x) -> bool {
+        return x % 2 == 0;
+    });
+    CHECK(expected == input);
 }
 
 TEST_CASE("util::generic::reverse_if")
@@ -16,6 +28,7 @@ TEST_CASE("util::generic::reverse_if")
     std::string input = "hh";
     std::string expected = "hh";
     util::generic::reverse_if(input.begin(), input.end(), std::isdigit);
+    CHECK(expected == input);
 }
 
 TEST_CASE("util::generic::reverse_if")
@@ -23,6 +36,7 @@ TEST_CASE("util::generic::reverse_if")
     std::string input = "34";
     std::string expected = "43";
     util::generic::reverse_if(input.begin(), input.end(), std::isdigit);
+    CHECK(expected == input);
 }
 
 TEST_CASE("util::generic::reverse_if")
@@ -30,6 +44,7 @@ TEST_CASE("util::generic::reverse_if")
     std::string input = "";
     std::string expected = "";
     util::generic::reverse_if(input.begin(), input.end(), std::isdigit);
+    CHECK(expected == input);
 }
 
 TEST_CASE("util::generic::reverse_if")
@@ -37,6 +52,7 @@ TEST_CASE("util::generic::reverse_if")
     std::string input = "3";
     std::string expected = "3";
     util::generic::reverse_if(input.begin(), input.end(), std::isdigit);
+    CHECK(expected == input);
 }
 
 TEST_CASE("util::generic::reverse_if")
@@ -44,4 +60,5 @@ TEST_CASE("util::generic::reverse_if")
     std::string input = "4";
     std::string expected = "4";
     util::generic::reverse_if(input.begin(), input.end(), std::isdigit);
+    CHECK(expected == input);
 }
