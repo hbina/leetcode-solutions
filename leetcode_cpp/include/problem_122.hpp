2154a571af34030580dee6eaa6862831e66660cb
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-12-30 03:37:42 -0500

    Solved problem 121 and 122

---
+++ leetcode_cpp/include/problem_122.hpp
@@ -0,0 +1,35 @@
+#include <iterator>
+#include <algorithm>
+
+namespace leetcode
+{
+
+template <
+    typename Iterable,
+    typename T = typename std::iterator_traits<
+        typename Iterable::iterator>::value_type>
+static constexpr T
+maxProfit_Sum(const Iterable &prices)
+{
+    T acc = T{};
+    auto range = std::cbegin(prices);
+    do
+    {
+        auto range_begin = range;
+        range = std::is_sorted_until(
+            range,
+            std::cend(prices),
+            [](const auto &lhs, const auto &rhs) -> bool {
+                return lhs < rhs;
+            });
+        auto [min, max] = std::minmax_element(range_begin, range);
+        if (min != max)
+        {
+            acc += *max - *min;
+        }
+
+    } while (range != std::cend(prices));
+    return acc;
+}
+
+}; // namespace leetcode
