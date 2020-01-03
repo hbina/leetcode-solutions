04b0240d04ddf7d01d691c7509a26986fdb0d148
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-01-03 03:43:31 -0500

    Simplified implementation of getAllElements

--- leetcode_cpp/include/leetcode/problem_1305.hpp
+++ leetcode_cpp/include/leetcode/problem_1305.hpp
@@ -12,36 +12,6 @@ namespace leetcode
 {
 
 template <
-    typename T>
-static std::vector<T>
-getAllElements(
-    const TreeNode<T> *lhs,
-    const TreeNode<T> *rhs)
-{
-    std::vector<T> result;
-
-    leetcode::BSTIterator<T> lhs_iterator =
-        leetcode::BSTIterator<T>(lhs);
-    leetcode::BSTIterator<T> rhs_iterator =
-        leetcode::BSTIterator<T>(rhs);
-
-    while (lhs_iterator.hasNext() && rhs_iterator.hasNext())
-    {
-        //  Can be replaced with a Comp
-        if (lhs_iterator.peek()->val < rhs_iterator.peek()->val)
-            result.push_back(lhs_iterator.next()->val);
-        else
-            result.push_back(rhs_iterator.next()->val);
-    }
-    while (lhs_iterator.hasNext())
-        result.push_back(lhs_iterator.next()->val);
-    while (rhs_iterator.hasNext())
-        result.push_back(rhs_iterator.next()->val);
-
-    return result;
-}
-
-template <
     typename T,
     typename BinaryPredicate>
 static std::vector<T>
@@ -71,6 +41,16 @@ getAllElements_If(
         result.push_back(rhs_iterator.next()->val);
 
     return result;
+}
+
+template <
+    typename T>
+static std::vector<T>
+getAllElements(
+    const TreeNode<T> *lhs,
+    const TreeNode<T> *rhs)
+{
+    return getAllElements_If(lhs, rhs, std::less{});
 }
 
 }; // namespace leetcode
